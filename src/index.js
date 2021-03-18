ed6a489cefb0d049c89b164a8a87395711f9026d
DRBriseno <rose.briseno@students.makeschool.com>
2021-03-17 17:44:43 -0700

    + info to HTML with Java

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-//navbarjs
-$(function() {
-    $('body').addClass('js');
-  
-    var $hamburger = $('.hamburger'),
-        $nav = $('#site-nav'),
-        $masthead = $('#masthead');
-  
-    $hamburger.click(function() {
-      $(this).toggleClass('is-active');
-      $nav.toggleClass('is-active');
-      $masthead.toggleClass('is-active');
-      return false; 
-    })
-});
+import data from "./data.js";
 
 // Create the HTML elements needed for a card
 const div = document.createElement("div");
 const h4 = document.createElement("h4");
 const a = document.createElement("a");
 const img = document.createElement("img");
+
 
 // find the body element so that the newly created elements
 // can be appended to it
@@ -28,17 +15,38 @@ const body = document.querySelector("body");
 
 // add the div to the body
 body.append(div);
-
 // Add the anchor tag inside of the h4
 h4.append(a);
-
 // Add the h4 inside the div
 div.append(h4);
-
 // Add the img inside the div
 div.append(img);
 
+// use the first puppy in data
+const puppy = data[0];
+
+// Use innerHTML to add text to the anchor tag
+a.innerHTML = puppy.name;
+
+// use setAttribute to add the card class to the div, etc
+div.setAttribute("class", "card");
+a.setAttribute("href", puppy.info);
+img.setAttribute("src", puppy.pic);
 
 
 
-a.innerHTML = "Meg-The-Pug"
+//navbarjs
+$(function() {
+  $('body').addClass('js');
+
+  var $hamburger = $('.hamburger'),
+      $nav = $('#site-nav'),
+      $masthead = $('#masthead');
+
+  $hamburger.click(function() {
+    $(this).toggleClass('is-active');
+    $nav.toggleClass('is-active');
+    $masthead.toggleClass('is-active');
+    return false; 
+  })
+});
