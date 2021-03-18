f3d4fc16dc5fb6493fe249037bf823e8d9fe9da7
DRBriseno <rose.briseno@students.makeschool.com>
2021-03-17 18:33:30 -0700

    Attempting to dynamically build entire list, no such luck yet

--- src/index.js
+++ src/index.js
@@ -1,52 +1,31 @@
 import data from "./data.js";
 
-// Create the HTML elements needed for a card
-const div = document.createElement("div");
-const h4 = document.createElement("h4");
-const a = document.createElement("a");
-const img = document.createElement("img");
+
+const buildPuppyCard = function(puppy){
+  // Elements needed to build a card  
+    const div = document.createElement('div')
+    const h4 = document.createElement('h4')
+    const a = document.createElement('a')
+    const img = document.createElement('img')
 
 
-// find the body element so that the newly created elements
-// can be appended to it
-const body = document.querySelector("body");
+  // Newly created elements in the DOM
+    const body = document.querySelector('body');
+    body.append(div)
+    h4.append(a)
+    div.append(h4)
+    div.append(img)
 
-// Append the elements to the DOM
 
-// add the div to the body
-body.append(div);
-// Add the anchor tag inside of the h4
-h4.append(a);
-// Add the h4 inside the div
-div.append(h4);
-// Add the img inside the div
-div.append(img);
-
-// use the first puppy in data
-const puppy = data[0];
-
-// Use innerHTML to add text to the anchor tag
-a.innerHTML = puppy.name;
-
-// use setAttribute to add the card class to the div, etc
-div.setAttribute("class", "card");
-a.setAttribute("href", puppy.info);
-img.setAttribute("src", puppy.pic);
+  // Content and attributes
+    a.innerHTML = puppy.name
+    a.setAttribute('href', puppy.info)
+    img.setAttribute('src', puppy.pic)
+    div.setAttribute('class', 'card')
+  }
 
 
 
-//navbarjs
-$(function() {
-  $('body').addClass('js');
-
-  var $hamburger = $('.hamburger'),
-      $nav = $('#site-nav'),
-      $masthead = $('#masthead');
-
-  $hamburger.click(function() {
-    $(this).toggleClass('is-active');
-    $nav.toggleClass('is-active');
-    $masthead.toggleClass('is-active');
-    return false; 
-  })
-});
+data.forEach(function(puppy) {
+  buildPuppyCard(puppy)
+}
