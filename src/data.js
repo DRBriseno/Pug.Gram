623130815f0dfd199f655bb3974c4d3e79cbe64a
DRBriseno <rose.briseno@students.makeschool.com>
2021-03-18 21:09:10 -0700

    Uncaught SyntaxError: Cannot use import statement outside a module / still appearing after trying several fixes

--- src/data.js
+++ src/data.js
@@ -1,95 +1,73 @@
-const data = [
-    {
-      id: 1,
-      pic:
-      "https://images.unsplash.com/photo-1529927066849-79b791a69825?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8cHVnfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Meg The Pug",
-      breed: "Pug",
-      info:
-      "https://www.instagram.com/pugmegthe/"
+const data = [{
+        id: 1,
+        pic: "https://images.unsplash.com/photo-1529927066849-79b791a69825?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8cHVnfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Meg The Pug",
+        breed: "Pug",
+        info: "https://www.instagram.com/pugmegthe/"
     },
     {
-      id: 2,
-      pic:
-        "https://images.unsplash.com/photo-1519098901909-b1553a1190af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8Y29yZ2l8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Ralph the Corgi",
-      breed: "Corgi",
-      info:
-      "https://www.instagram.com/ralphthecorgi/?hl=en",
+        id: 2,
+        pic: "https://images.unsplash.com/photo-1519098901909-b1553a1190af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8Y29yZ2l8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Ralph the Corgi",
+        breed: "Corgi",
+        info: "https://www.instagram.com/ralphthecorgi/?hl=en",
     },
     {
-      id: 3,
-      pic:
-        "https://www.instagram.com/p/B7zUaPkpDSW/?utm_source=ig_web_button_share_sheet",
-      name: "Nova May",
-      breed: "Australian Shepherd",
-      info:
-        "https://www.instagram.com/seekingsupernova/"
+        id: 3,
+        pic: "https://www.instagram.com/p/B7zUaPkpDSW/?utm_source=ig_web_button_share_sheet",
+        name: "Nova May",
+        breed: "Australian Shepherd",
+        info: "https://www.instagram.com/seekingsupernova/"
     },
     {
-      id: 4,
-      pic:
-        "https://images.unsplash.com/photo-1613518082705-a4e036f1ce08?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fGZyZW5jaCUyMGJ1bGxkb2d8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Manny the Frenchie",
-      breed: "French Bulldog",
-      info:
-        "https://www.instagram.com/manny_the_frenchie/"
+        id: 4,
+        pic: "https://images.unsplash.com/photo-1613518082705-a4e036f1ce08?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjR8fGZyZW5jaCUyMGJ1bGxkb2d8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Manny the Frenchie",
+        breed: "French Bulldog",
+        info: "https://www.instagram.com/manny_the_frenchie/"
     },
     {
-      id: 5,
-      pic:
-        "https://images.unsplash.com/photo-1455103493930-a116f655b6c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGl0YnVsbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Roofus & Kilo",
-      breed: "Pitbull",
-      info:
-        "https://www.instagram.com/roofusandkilo/"
+        id: 5,
+        pic: "https://images.unsplash.com/photo-1455103493930-a116f655b6c5?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8cGl0YnVsbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Roofus & Kilo",
+        breed: "Pitbull",
+        info: "https://www.instagram.com/roofusandkilo/"
     },
     {
-      id: 6,
-      pic:
-        "https://images.unsplash.com/photo-1517982990603-1d52f060fe6a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl0YnVsbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Kingston",
-      breed: "Pitbull",
-      info:
-        "https://www.instagram.com/hellokingston/"
+        id: 6,
+        pic: "https://images.unsplash.com/photo-1517982990603-1d52f060fe6a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGl0YnVsbHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Kingston",
+        breed: "Pitbull",
+        info: "https://www.instagram.com/hellokingston/"
     },
     {
-      id: 7,
-      pic:
-        "https://images.unsplash.com/photo-1596957901846-a0722f546502?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlbmNoJTIwYnVsbGRvZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Boba the Frenchie",
-      breed: "French Bulldog",
-      info:
-        "https://www.instagram.com/boba_the_cute_frenchie/"
+        id: 7,
+        pic: "https://images.unsplash.com/photo-1596957901846-a0722f546502?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlbmNoJTIwYnVsbGRvZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Boba the Frenchie",
+        breed: "French Bulldog",
+        info: "https://www.instagram.com/boba_the_cute_frenchie/"
     },
     {
-      id: 8,
-      pic:
-      "https://images.unsplash.com/photo-1558528395-501d52684c78?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGF1c3RyYWxpYW4lMjBzaGVwaGVyZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Kimchi the Aussie",
-      breed: "Australian Shepherd",
-      info:
-        "https://www.instagram.com/kimchi_theaussie/"
+        id: 8,
+        pic: "https://images.unsplash.com/photo-1558528395-501d52684c78?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGF1c3RyYWxpYW4lMjBzaGVwaGVyZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Kimchi the Aussie",
+        breed: "Australian Shepherd",
+        info: "https://www.instagram.com/kimchi_theaussie/"
     },
     {
-      id: 9,
-      pic:
-        "https://images.unsplash.com/photo-1561852184-92b9bb821045?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fGNvcmdpfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Geordi La Corgi & Scotty",
-      breed: "Corgi",
-      info:
-        "https://www.instagram.com/lacorgi/"
+        id: 9,
+        pic: "https://images.unsplash.com/photo-1561852184-92b9bb821045?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fGNvcmdpfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Geordi La Corgi & Scotty",
+        breed: "Corgi",
+        info: "https://www.instagram.com/lacorgi/"
     },
     {
-      id: 10,
-      pic:
-        "https://images.unsplash.com/photo-1528773002192-2ed790ee2c48?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fHB1Z3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
-      name: "Lou Lou the Pug",
-      breed: "Pug",
-      info:
-        "https://www.instagram.com/pugloulou/"
+        id: 10,
+        pic: "https://images.unsplash.com/photo-1528773002192-2ed790ee2c48?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fHB1Z3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        name: "Lou Lou the Pug",
+        breed: "Pug",
+        info: "https://www.instagram.com/pugloulou/"
     }
-  ];
-  
-  export default data;
-  
+];
+
+export default data;
--- src/index.js
+++ src/index.js
@@ -1,86 +1,87 @@
-import data from "src/data.js";
+import data from "./data.js";
+
 
 const addPuppy = e => {
-  // prevent page from refreshing on submit
-  e.preventDefault();
+    // prevent page from refreshing on submit
+    e.preventDefault();
 
 
-  // Get input elements from the form
-  let newPupName = document.getElementById("PuppyName").value;
-  let newPupInfo = document.getElementById("IGUrl").value;
-  let newPupPic = document.getElementById("PhotoURL").value;
-  let newPupDif = document.getElementById("diff").value;
+    // Get input elements from the form
+    let newPupName = document.getElementById("PuppyName").value;
+    let newPupInfo = document.getElementById("IGUrl").value;
+    let newPupPic = document.getElementById("PhotoURL").value;
+    let newPupDif = document.getElementById("diff").value;
 
 
-  // format values into a new object
-  const newPup = {
-    name: newPupName.value,
-    info: newPupInfo.value,
-    pic: newPupPic.value,
-    breed: newPupDif.value,
-    id: data.length
-  };
+    // format values into a new object
+    const newPup = {
+        name: newPupName.value,
+        info: newPupInfo.value,
+        pic: newPupPic.value,
+        breed: newPupDif.value,
+        id: data.length
+    };
 
 
-  const div = document.createElement("div");
-  const h4 = document.createElement("h4");
-  const a = document.createElement("a");
-  const img = document.createElement("img");
-  const body = document.querySelector(".puppy-container");
-  div.appendChild(h4);
-  h4.appendChild(a);
-  div.appendChild(img);
+    const div = document.createElement("div");
+    const h4 = document.createElement("h4");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    const body = document.querySelector(".puppy-container");
+    div.appendChild(h4);
+    h4.appendChild(a);
+    div.appendChild(img);
 
 
-  body.append(div);
-  a.textContent = puppy.name;
-  div.classList.add("card");
-  a.setAttribute("href", puppy.info);
-  img.setAttribute("src", puppy.pic);
+    body.append(div);
+    a.textContent = puppy.name;
+    div.classList.add("card");
+    a.setAttribute("href", puppy.info);
+    img.setAttribute("src", puppy.pic);
 
-  // Clear the form
-  newPupName.value = "";
-  newPupInfo.value = "";
-  newPupPic.value = "";
-  newPupDif.value = "";
+    // Clear the form
+    newPupName.value = "";
+    newPupInfo.value = "";
+    newPupPic.value = "";
+    newPupDif.value = "";
 };
 
 const button = document.querySelector('button[type="submit"]');
 button.addEventListener("click", addPuppy);
 
 const buildPuppyCard = puppy => {
-  // elements needed to build a card
-  const div = document.createElement("div");
-  const h4 = document.createElement("h4");
-  const a = document.createElement("a");
-  const img = document.createElement("img");
+    // elements needed to build a card
+    const div = document.createElement("div");
+    const h4 = document.createElement("h4");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
 
-  // append newly created elements into the DOM
-  const body = document.querySelector(".puppy-container");
-  body.append(div);
-  h4.append(a);
-  div.append(h4);
-  div.append(img);
+    // append newly created elements into the DOM
+    const body = document.querySelector(".puppy-container");
+    body.append(div);
+    h4.append(a);
+    div.append(h4);
+    div.append(img);
 
-  // content and attributes
-  a.innerHTML = puppy.name;
-  a.setAttribute("href", puppy.info);
-  img.setAttribute("src", puppy.pic);
-  div.setAttribute("class", "card");
+    // content and attributes
+    a.innerHTML = puppy.name;
+    a.setAttribute("href", puppy.info);
+    img.setAttribute("src", puppy.pic);
+    div.setAttribute("class", "card");
 
-  // edit btn and form for each pup
-  const editBtn = document.createElement("button");
-  editBtn.innerText = "Edit";
-  editBtn.classList.add("edit-btn" + puppy.id);
+    // edit btn and form for each pup
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    editBtn.classList.add("edit-btn" + puppy.id);
 
-  editBtn.addEventListener("click", () => editStyle(puppy));
+    editBtn.addEventListener("click", () => editStyle(puppy));
 
-  const editForm = document.createElement("form");
-  editForm.classList.add("hidden");
+    const editForm = document.createElement("form");
+    editForm.classList.add("hidden");
 
-  // unique id
-  editForm.classList.add("edit-form" + puppy.id, "edit-form");
-  editForm.innerHTML = `
+    // unique id
+    editForm.classList.add("edit-form" + puppy.id, "edit-form");
+    editForm.innerHTML = `
   
     <label for="pupname">Puppy Name:</label><input id="pupname"<br>
     <input type="text" id="pupname" name="pupname"><br>
@@ -101,59 +102,59 @@ const buildPuppyCard = puppy => {
     </select>
      
   `;
-  div.append(editBtn);
-  div.append(editForm);
+    div.append(editBtn);
+    div.append(editForm);
 };
 
 const editStyle = puppy => {
-  
-  const editForm = document.querySelector(".edit-form" + puppy.id);
-  editForm.classList.toggle("hidden");
 
-  const editBtn = document.querySelector(".edit-btn" + puppy.id);
-
-  // reflect this when in edit mode
-  editBtn.innerText = "Done Editing";
-
-  // event listener to the edit button
-  // take user input and update the UI
-  // reflect the user's changes
-  editBtn.addEventListener("click", () => {
     const editForm = document.querySelector(".edit-form" + puppy.id);
-
-    // new puppy object with the users changes
-    // replace the previous pup info
-    // values are either be new user input/default values if not changed by user
-    const editedPupName =
-      editForm.querySelector("input#name").value || puppy.name;
-    const editedPupInfo =
-      editForm.querySelector("input#infoUrl").value || puppy.info;
-    const editedPupPic =
-      editForm.querySelector("input#picture").value || puppy.pic;
-    const editedPupDif =
-      editForm.querySelector("select#diff").value || puppy.breed;
-
-    // new object that includes users changes
-    const updatedPuppy = {
-      name: editedPupName,
-      info: editedPupInfo,
-      pic: editedPupPic,
-      breed: editedPupDif,
-      id: puppy.id
-    };
-
-    data[puppy.id - 1] = updatedPuppy;
-
-    // puppy container in the HTML
-    // replace with a new list of puppy cards that include the updated puppy
-    const puppyContainer = document.querySelector(".puppy-container");
-    puppyContainer.innerHTML = "";
-    data.forEach(puppy => buildPuppyCard(puppy));
-
-   
     editForm.classList.toggle("hidden");
-    editBtn.innerText = "Edit";
-  });
+
+    const editBtn = document.querySelector(".edit-btn" + puppy.id);
+
+    // reflect this when in edit mode
+    editBtn.innerText = "Done Editing";
+
+    // event listener to the edit button
+    // take user input and update the UI
+    // reflect the user's changes
+    editBtn.addEventListener("click", () => {
+        const editForm = document.querySelector(".edit-form" + puppy.id);
+
+        // new puppy object with the users changes
+        // replace the previous pup info
+        // values are either be new user input/default values if not changed by user
+        const editedPupName =
+            editForm.querySelector("input#name").value || puppy.name;
+        const editedPupInfo =
+            editForm.querySelector("input#infoUrl").value || puppy.info;
+        const editedPupPic =
+            editForm.querySelector("input#picture").value || puppy.pic;
+        const editedPupDif =
+            editForm.querySelector("select#diff").value || puppy.breed;
+
+        // new object that includes users changes
+        const updatedPuppy = {
+            name: editedPupName,
+            info: editedPupInfo,
+            pic: editedPupPic,
+            breed: editedPupDif,
+            id: puppy.id
+        };
+
+        data[puppy.id - 1] = updatedPuppy;
+
+        // puppy container in the HTML
+        // replace with a new list of puppy cards that include the updated puppy
+        const puppyContainer = document.querySelector(".puppy-container");
+        puppyContainer.innerHTML = "";
+        data.forEach(puppy => buildPuppyCard(puppy));
+
+
+        editForm.classList.toggle("hidden");
+        editBtn.innerText = "Edit";
+    });
 };
 
 
@@ -164,17 +165,17 @@ deleteBtn.innerText = "Delete";
 div.append(deleteBtn);
 
 deleteBtn.addEventListener("click", () => {
-  deletePuppy(puppy.id));
-}
+    deletePuppy(puppy.id);
 
-const deletePuppy = puppyId => {
-  let nonDeletedPuppy = data.filter(puppy => {
-    return puppy.id !== puppyId;
-  });
- const puppyContainer = document.querySelector(".hairstyle-container");
- puppyContainer.innerHTML = "";
-  nonDeletedPuppy.forEach(puppy => {    
-    buildPuppyCard(puppy));
-}
-};
 
+    const deletePuppy = puppyId => {
+        let nonDeletedPuppy = data.filter(puppy => {
+            return puppy.id !== puppyId;
+        });
+        const puppyContainer = document.querySelector(".puppy-container");
+        puppyContainer.innerHTML = "";
+        nonDeletedPuppy.forEach(puppy => {
+            buildPuppyCard(puppy);
+        })
+    }
+})
